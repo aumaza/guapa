6a5871c89f152d45141deff36dcb7726eaf5d414
Augusto Maza <debianmaza@gmail.com>
2025-03-19 14:36:47 -0300

    commit #6 se sube nuevo dump de la base de datos [ 19-03-2025 ]

--- dump_data_base.sh
+++ dump_data_base.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 fecha=`date +%d-%m-%Y`
 archivo="sqls/dumps/guapa-$fecha.sql"
-mysqldump --user=root --password=slack142 --host=slackzone.ddns.net guapa > $archivo
-#mysqldump --user=root --password=slack142 --host=localhost gesdoju > $archivo
+#mysqldump --user=root --password=slack142 --host=slackzone.ddns.net guapa > $archivo
+mysqldump --user=root --password=slack142 --host=localhost guapa > $archivo
 chmod 777 $archivo
 
 
